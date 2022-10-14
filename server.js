7000d1b25f2957a1bba45d28b475c25dee8aac7a
DylanMacres <Dylan.Macres@du.edu>
2022-10-13 22:44:48 -0600

    adding my prompts to ask the user

--- server.js
+++ server.js
@@ -1,5 +1,41 @@
 const inquirer = require("inquirer")
 // const db 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const { execArgv } = require("process");
+const { response } = require("express");
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+function startQuestions() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message:"Select from the list",
+            choices: [
+                "View Roles",
+                "View Departments",
+                "View Employees",
+                "Add Role",
+                "Add New Employee",
+                "Add Department",
+                "Finish",
+            ],
+        }   
+    ])
+
+
+    .then((response) => {
+        
+    })
+}
