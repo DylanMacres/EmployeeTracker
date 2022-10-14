37fc39337b3ce9e13b593bb5788949cee96a2b52
DylanMacres <Dylan.Macres@du.edu>
2022-10-14 00:19:38 -0600

    finished schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department{
 
 
 CREATE TABLE role{
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(30) NOT NULL, 
+  id INT NOT NULL AUTO_INCREMENT, title VARCHAR(30) NOT NULL, 
   salary DECIMAL NOT NULL,
   department_id INT,
   PRIMARY KEY (id),
@@ -20,4 +20,18 @@ CREATE TABLE role{
   REFERENCES departments(id) ON DELETE SET NULL,
 };
 
-CREATE TABLE employee 
+CREATE TABLE employee {
+ id INT NOT NULL AUTO_INCREMENT,
+ first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+ role_id INT, 
+ manager_id INT,
+ manager_id VARCHAR(30) DEFAULT NULL, 
+ REFERENCES employee(id)
+ FOREIGN KEY (role_id)
+ ON DELETE SET NULL,
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL,
+ 
+};
